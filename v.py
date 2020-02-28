9941854cfaa1f62b4d02d5504a400fd055ede7ef
Wei Gao <cdspit@163.com>
2020-02-28 20:47:55 +0800

    update py,vim info

---
+++ v.py
@@ -0,0 +1,53 @@
+#___________  print hello world  ___________#
+print("HEllo world!")   
+
+#____________ variate  ___________#
+message = "Hello World!"
+print(message)
+message = "Hello Python Crash Course World!"
+print(message)
+
+#____________ string _____________#
+name = "zhangfuhua lovelace"
+print(name.title())
+name = "ZHANGFUHUA LOVELACE"
+print(name.title())
+name = "ZHANgfuhua lovelaCE"
+print(name.upper())
+print(name.lower())
+# merge #
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+print(full_name)
+print(full_name.title())
+# delete right null or left null
+favorite_language = '    Python is good language   '
+print(favorite_language)
+print(favorite_language.rstrip()) #delete right
+print(favorite_language.lstrip()) #delete left  
+print(favorite_language.strip())  #delete both    
+# exercise #
+name = "Xi Wang"
+message = "Hello," + name + ",you are so nic!"
+print(message)
+print(name.title())
+print(name.upper())
+print(name.lower())
+#________________________ number _________________#
+print(2 * 4)
+print(2 ** 3)
+print(2+6  )
+print(1+7 )
+
+
+
+
+
+
+
+
+
+
+
+
